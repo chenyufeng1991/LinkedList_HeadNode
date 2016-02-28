3cb9a7a8e9dd2b230b4d58c6d5da30f98da120b6
chenyufeng <503434355@qq.com>
2016-02-28 16:01:43 +0800

    Initial Commit

---
+++ LinkedList_HeadNode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinkedList_HeadNode
+//
+//  Created by chenyufeng on 16/2/28.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
